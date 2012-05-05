bd9321a0a848d42f940957d80364ec71a312124d
Erik Erskine <erik@brightec.co.uk>
2012-05-05 18:13:04 +0100

    tabs to spaces

--- alpha-conferences/src/uk/co/brightec/alphaconferences/rows/ProgrammeRow.java
+++ alpha-conferences/src/uk/co/brightec/alphaconferences/rows/ProgrammeRow.java
@@ -7,55 +7,55 @@ import android.view.View;
 import android.widget.TextView;
 
 public class ProgrammeRow extends Row {
-	public String mTitle;
-	public String mVenue;
-	public String mSpeakerName;
-	public String mTime;
-	public int mBarColour;
-	
-	
-	public ProgrammeRow(String title, String venue, String speakerName, String time, int barColour, Context context) {
-		super(context);
-		mTitle = title;
-		mVenue = venue;
-		mSpeakerName = speakerName;
-		mTime = time;
-		mBarColour = barColour;
-	}	
-	
-	
-	@Override
-	public View getView(View convertView) {
-		View rowView = convertView;
-		if (rowView == null) {
-			rowView = mInflater.inflate(R.layout.programme_list_item, null);
-			ProgrammeViewHolder holder = new ProgrammeViewHolder();
-			holder.barColourView = rowView.findViewById(R.id.barColour);
-			holder.titleTextView = (TextView)rowView.findViewById(R.id.title);
-			holder.subTitleTextView = (TextView)rowView.findViewById(R.id.subTitle);
-			holder.speakerNameTextView = (TextView)rowView.findViewById(R.id.speakerName);
-			holder.timeTextView = (TextView)rowView.findViewById(R.id.time);
-			rowView.setTag(holder);
-		}
-		
-		ProgrammeViewHolder holder = (ProgrammeViewHolder)rowView.getTag();			
-		holder.barColourView.setBackgroundColor(mBarColour);
-		holder.titleTextView.setText(mTitle);
-		holder.subTitleTextView.setText(mVenue);
-		holder.speakerNameTextView.setText(mSpeakerName);
-		holder.timeTextView.setText(mTime);
-		
-		return rowView;
-	}
-	
-	
-	private static class ProgrammeViewHolder {
-		View barColourView;
-		TextView titleTextView;
-		TextView subTitleTextView;
-		TextView speakerNameTextView;
-		TextView timeTextView;
-	}	
+    public String mTitle;
+    public String mVenue;
+    public String mSpeakerName;
+    public String mTime;
+    public int mBarColour;
 
-	
+
+    public ProgrammeRow(String title, String venue, String speakerName, String time, int barColour, Context context) {
+        super(context);
+        mTitle = title;
+        mVenue = venue;
+        mSpeakerName = speakerName;
+        mTime = time;
+        mBarColour = barColour;
+    }
+
+
+    @Override
+    public View getView(View convertView) {
+        View rowView = convertView;
+        if (rowView == null) {
+            rowView = mInflater.inflate(R.layout.programme_list_item, null);
+            ProgrammeViewHolder holder = new ProgrammeViewHolder();
+            holder.barColourView = rowView.findViewById(R.id.barColour);
+            holder.titleTextView = (TextView)rowView.findViewById(R.id.title);
+            holder.subTitleTextView = (TextView)rowView.findViewById(R.id.subTitle);
+            holder.speakerNameTextView = (TextView)rowView.findViewById(R.id.speakerName);
+            holder.timeTextView = (TextView)rowView.findViewById(R.id.time);
+            rowView.setTag(holder);
+        }
+
+        ProgrammeViewHolder holder = (ProgrammeViewHolder)rowView.getTag();
+        holder.barColourView.setBackgroundColor(mBarColour);
+        holder.titleTextView.setText(mTitle);
+        holder.subTitleTextView.setText(mVenue);
+        holder.speakerNameTextView.setText(mSpeakerName);
+        holder.timeTextView.setText(mTime);
+
+        return rowView;
+    }
+
+
+    private static class ProgrammeViewHolder {
+        View barColourView;
+        TextView titleTextView;
+        TextView subTitleTextView;
+        TextView speakerNameTextView;
+        TextView timeTextView;
+    }
+
+
 }
