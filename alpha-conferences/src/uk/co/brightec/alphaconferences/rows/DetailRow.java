7af0306f646b5d6266049013cd54e63100c7145a
Erik Erskine <erik@brightec.co.uk>
2012-05-05 16:21:03 +0100

    DetailRow now uses a DownloadableImageView added biography and website/twitter buttons to SpeakerDetailActivity

--- alpha-conferences/src/uk/co/brightec/alphaconferences/rows/DetailRow.java
+++ alpha-conferences/src/uk/co/brightec/alphaconferences/rows/DetailRow.java
@@ -1,19 +1,21 @@
 package uk.co.brightec.alphaconferences.rows;
 
 import uk.co.brightec.alphaconferences.Cell;
+import uk.co.brightec.alphaconferences.DownloadableImageView;
 import uk.co.brightec.alphaconferences.R;
 import uk.co.brightec.alphaconferences.Row;
+import uk.co.brightec.alphaconferences.resources.Resource;
 import android.content.Context;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DetailRow extends Row implements Cell {
+    
 	private final String mTitle;
 	private final String mSubTitle;
-	public int mImageResource;
+	public final Resource mImageResource;
 	
-	protected DetailRow(String title, String subTitle, int imageResource, Context context) {
+	public DetailRow(String title, String subTitle, Resource imageResource, Context context) {
 		super(context);
 		mTitle = title;
 		mSubTitle = subTitle;
@@ -30,14 +32,17 @@ public class DetailRow extends Row implements Cell {
 			DetailViewHolder holder = new DetailViewHolder();
 			holder.titleTextView = (TextView)rowView.findViewById(R.id.titleTextView);
 			holder.subTitleTextView = (TextView)rowView.findViewById(R.id.subTitleTextView);
-			holder.imageView = (ImageView)rowView.findViewById(R.id.imageView);
+			holder.imageView = (DownloadableImageView)rowView.findViewById(R.id.imageView);
 			rowView.setTag(holder);
 		}
 		
 		DetailViewHolder holder = (DetailViewHolder)rowView.getTag();			
 		holder.titleTextView.setText(mTitle);
 		holder.subTitleTextView.setText(mSubTitle);
-		holder.imageView.setImageResource(mImageResource);
+		
+		if (mImageResource != null) {
+		    holder.imageView.setUrl(mImageResource.url(), mImageResource.cacheFilename());
+		}
 		
 		return rowView;
 	}
@@ -45,6 +50,6 @@ public class DetailRow extends Row implements Cell {
 	private static class DetailViewHolder {
 		TextView titleTextView;
 		TextView subTitleTextView;
-		ImageView imageView;		
+		DownloadableImageView imageView;		
 	}	
 }
--- alpha-conferences/src/uk/co/brightec/alphaconferences/speakers/SpeakerDetailActivity.java
+++ alpha-conferences/src/uk/co/brightec/alphaconferences/speakers/SpeakerDetailActivity.java
@@ -1,19 +1,33 @@
 package uk.co.brightec.alphaconferences.speakers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import uk.co.brightec.alphaconferences.AlphaAdapter;
+import uk.co.brightec.alphaconferences.Row;
 import uk.co.brightec.alphaconferences.data.DataStore;
 import uk.co.brightec.alphaconferences.data.Speaker;
+import uk.co.brightec.alphaconferences.resources.Resource;
+import uk.co.brightec.alphaconferences.rows.ButtonBarRow;
+import uk.co.brightec.alphaconferences.rows.DetailRow;
+import uk.co.brightec.alphaconferences.rows.HTMLRow;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 import com.actionbarsherlock.app.ActionBar;
-import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.SherlockListActivity;
 import com.actionbarsherlock.view.MenuItem;
 
 
-public class SpeakerDetailActivity extends SherlockActivity {
+public class SpeakerDetailActivity extends SherlockListActivity {
     
     static final String EXTRA_SPEAKER_ID = "EXTRA_SPEAKER_ID";
     
     private ActionBar mActionBar;
+    private Speaker speaker;
     
     
     @Override
@@ -21,11 +35,58 @@ public class SpeakerDetailActivity extends SherlockActivity {
         super.onCreate(savedInstanceState);
         
         int speakerId = getIntent().getIntExtra(EXTRA_SPEAKER_ID, 0);
-        Speaker s = DataStore.speaker(this, speakerId);
+        speaker = DataStore.speaker(this, speakerId);
         
         mActionBar = getSupportActionBar(); 
-        mActionBar.setTitle(s.displayName());
-        mActionBar.setDisplayHomeAsUpEnabled(true);         
+        mActionBar.setTitle(speaker.displayName());
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+        
+        AlphaAdapter adapter = new AlphaAdapter();
+        //getListView().setAdapter(adapter);
+        
+        setListAdapter(adapter);
+    }
+    
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        populate();
+    }
+
+    
+    private void populate() {
+        List<Row> rows = new ArrayList<Row>();
+        rows.add(new DetailRow(speaker.displayName(), speaker.position, new Resource(speaker.imageKey, Resource.Type.SpeakerImageSmall), this));
+        rows.add(new HTMLRow(speaker.biography, this));
+
+        OnClickListener websiteOnClick = null;
+        if (speaker.websiteUrl != null) {
+            websiteOnClick = new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(speaker.websiteUrl)));
+                }
+            };
+        }
+
+        OnClickListener twitterOnClick = null;
+        if (speaker.twitterUsername != null) {
+            twitterOnClick = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String url = "http://twitter.com/"+speaker.twitterUsername;
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                }
+            };
+        }
+
+        ButtonBarRow buttons = new ButtonBarRow(this);
+        buttons.setButton1("Website", websiteOnClick);
+        buttons.setButton2("Twitter", twitterOnClick);
+        rows.add(buttons);
+
+        ((AlphaAdapter) getListAdapter()).setRows(rows, this);
     }
     
     
@@ -34,16 +95,11 @@ public class SpeakerDetailActivity extends SherlockActivity {
         switch (item.getItemId()) {
         case android.R.id.home:
             finish();
-//            Intent intent = new Intent(this, MainActivity.class);
-//            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//            startActivity(intent);
             return true;
         default:
             return super.onOptionsItemSelected(item);
         }
     }
 
-    
-    
-    
+
 }
